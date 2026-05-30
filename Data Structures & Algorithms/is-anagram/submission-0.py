cb33c89352e76db354d4971ebf33a3168859fb7a
Thiyane Xavier <thiyanexavier6@gmail.com>
2026-05-30 22:48:15 +0800

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if  sorted(s)==sorted(t):
+            return True
+        else:
+            return False
+        
+
+s="racecar"
+t = "carrace"
+
+sol= Solution()
+
+print(sol.isAnagram(s,t))
